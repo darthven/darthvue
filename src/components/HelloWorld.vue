d1231f936a05d6e8ffe20e66ec5422c902db3a26
darthven <kostharl@gmail.com>
2019-07-14 13:28:33 +0300

    Vuetify was added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <div class="hello">
+          <h1>{{ msg }}</h1>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
 
 export default Vue.extend({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-});
+})
 </script>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { VApp,  VContainer, VLayout, VFlex } from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
+  },
+  options: {
+    customProperties: true,
+  },
+  iconfont: 'md',
+  components: {
+    VApp,
+    VContainer,
+    VLayout,
+    VFlex,
+  },
+})
